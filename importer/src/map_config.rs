11d93264694a1d48fc265d68f3744c0ead9e9303
Christopher Schmidt <crschmidt@crschmidt.net>
2021-12-24 10:16:37 -0500

    Adding Boston-area GTFS feed.

--- importer/src/map_config.rs
+++ importer/src/map_config.rs
@@ -77,6 +77,8 @@ pub fn config_for_map(name: &MapName) -> convert_osm::Options {
             Some("http://metro.kingcounty.gov/GTFS/google_transit.zip".to_string())
         } else if name.city == CityName::new("us", "san_francisco") {
             Some("https://gtfs.sfmta.com/transitdata/google_transit.zip".to_string())
+        } else if name.city == CityName::new("us", "boston") {
+            Some("https://cdn.mbta.com/MBTA_GTFS.zip".to_string())
         } else {
             None
         },
